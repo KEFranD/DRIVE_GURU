f46c911ce0dd32619df2848b1710a910b67a6c5d
Leo DHotman <dhotman19@yahoo.com>
2023-06-06 17:24:40 +0400

    don't merge please boss

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,7 @@
 class Instructor < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :car_transmission, presence: true
+  validates :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :student_bookings, class_name: 'Booking',
-                              foreign_key: 'student_id'
-  has_many :teacher_bookings, class_name: 'Booking',
-                              foreign_key: 'teacher_id'
+  has_many :instructors
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone_number, presence: true
-  validates :teacher_role, presence: true
-  validates :car_transmission, presence: true
-  validates :location, presence: true
 end
