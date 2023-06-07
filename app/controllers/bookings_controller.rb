dea7e5d9bc38672dafc4e6ced79b90197927e50d
alison frederic <alisonfrederic2@gmail.com>
2023-06-07 15:39:58 +0400

    bookingcontroller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,30 +1,44 @@
-class BookingsController < ApplicationController
-  before_action :set_user, only: %i[new create]
+class BookingsController < ApplicaionController
+  # before_action :set_bookings, only: %i[new create index show]
+  before_action :set_users, only: %i[new create index show]
+
+  def index
+    @bookings = Booking.all
+  end
+
   def new
-    @user = User.new(params[:id])
-    @instructor = Instructor.new(params[:id])
     @booking = Booking.new
+  end
+
+  def show
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @user = User.find(params[:instructor_id])
+
     @booking.user = @user
+    # @booking.instructor = @instructor
 
     if @booking.save
-      return successful save
+      redirect_to @booking, notice: 'Booking was successfully created.'
     else
-      return failure
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def set_user
-    @user = user.find(params[:user_id])
+    @user = User.find(params[:user_id])
   end
+
+  # def set_bookings
+  #   @booking = Instructor.find(params[:_id])
+  # end
 
   def booking_params
     params.require(:booking).permit(:start_time, :finish_time, :date)
   end
 end
-
