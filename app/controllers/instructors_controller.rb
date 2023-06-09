1ab541693d5325eb94cf161bcf85a893ad67125e
Francesca D <130538538+KEFranD@users.noreply.github.com>
2023-06-09 15:30:24 +0400

    added changes

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,4 +1,11 @@
 class InstructorsController < ApplicationController
   def show
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = []
+    marker = {
+      lat: @instructor.latitude,
+      lng: @instructor.longitude
+    }
+    @markers << marker
   end
 end
