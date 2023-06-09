cef3dd18a7a8bde01e6a4da4e6777eda29d9e8f7
Francesca D <130538538+KEFranD@users.noreply.github.com>
2023-06-09 17:00:01 +0400

    added stuff

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,4 +1,14 @@
 class InstructorsController < ApplicationController
   def show
+    @instructor = Instructor.find(params[:id])
+    @instructors = Instructor.where(location: @instructor.address)
   end
+
+  def index
+    @instructors = Instructor.all
+  if params[:query].present?
+    @instructors = @instructors.where("address ILIKE ?", "%#{params[:query]}%")
+  end
+  end
+
 end
