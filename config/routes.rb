d6cb4b0a3d584c4293d8a38ac9de9f795a579290
alison frederic <alisonfrederic2@gmail.com>
2023-06-09 11:18:58 +0400

    checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "checkout", to: "pages#checkout"
+  get "status", to: "pages#status"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :bookings do
-    resources :reviews, only: [:new, :create, :edit, :update]
-  end
-
+  resources :bookings
   resources :users, only: [:show] do
     resources :bookings, only: [:index]
+    resources :reviews, only: [:new, :create, :edit, :update]
+    resources :messages, only: [:index, :create]
   end
 
-  resources :messages, only: [:index, :create]
 end
